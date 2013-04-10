7d1e624d910133e8f91d3596d9d19b617fb7ecfa
Weiwei Chen <wchenpublic@gmail.com>
2013-04-09 23:53:29 -0700

    add comments

--- sources/org/workflowsim/utils/OverheadParameters.java
+++ sources/org/workflowsim/utils/OverheadParameters.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2007-2008 University Of Southern California
+ *  Copyright 2012-2013 University Of Southern California
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,80 +15,154 @@
  */
 package org.workflowsim.utils;
 
-import org.workflowsim.Job;
 import java.util.List;
 import java.util.Map;
 import org.cloudbus.cloudsim.Cloudlet;
 import org.cloudbus.cloudsim.Log;
+import org.workflowsim.Job;
 
 /**
- *
+ * This class includes all parameters that involve overheads
+ * 
  * @author Weiwei Chen
+ * @since WorkflowSim Toolkit 1.0
+ * @date Apr 9, 2013
  */
 public class OverheadParameters {
     
-    
+    /** The interval of workflow engine delay */
     private  int WED_INTERVAL;
     
+    /** The bandwidth */
     private double bandwidth;
     
-    private double random;
-    
-    private double random1;
-    
+
+    /** The list of workflow engine delay 
+     * key = level
+     * value = delay
+    */
     private Map<Integer, Double> WED_DELAY;
     
+    /** The list of queue delay 
+     * key = level
+     * value = delay
+    */
     private Map<Integer, Double> QUEUE_DELAY;
     
+    /** The list of postscript delay 
+     * key = level
+     * value = delay
+    */
     private Map<Integer, Double> POST_DELAY;
 
+    /** The list of clustering delay 
+     * key = level
+     * value = delay
+    */
     private Map<Integer, Double> CLUST_DELAY;
     
+    
+    /**
+	 * Created a new OverheadParameters object.
+	 * 
+	 * @param wed_interval, the interval of workflow engine 
+         * @param wed_delay, the list of workflow engine delay
+         * @param queue_delay, the list of queue delay
+         * @param post_delay, the list of postscript delay
+         * @param cluster_delay, the list of clustering delay
+         * @param bandwidth, the bandwidth
+	 * @pre $none
+	 * @post $none
+	 */
     public OverheadParameters(int wed_interval, 
             Map<Integer, Double> wed_delay, 
             Map<Integer, Double> queue_delay, 
             Map<Integer, Double> post_delay,
             Map<Integer, Double> cluster_delay, 
-            double bandwidth, double random, 
-            double random1){
+            double bandwidth){
         this.WED_INTERVAL   = wed_interval;
         this.WED_DELAY      = wed_delay;
         this.QUEUE_DELAY    = queue_delay;
         this.POST_DELAY     = post_delay;
         this.CLUST_DELAY    = cluster_delay;
         this.bandwidth      = bandwidth;
-        this.random         = random;
-        this.random1        = random1;
+
     }
     
-    public double getRandom(){
-        return this.random;
-    }
-    
-    public double getRandom1(){
-        return this.random1;
-    }
+    /**
+    * Gets the bandwidth
+    * 
+    * @return the bandwidth
+    * @pre $none
+    * @post $none
+    */
     public double getBandwidth(){
         return this.bandwidth;
     }
     
+    /**
+    * Gets the interval
+    * 
+    * @return the interval
+    * @pre $none
+    * @post $none
+    */
     public int getWEDInterval(){
         return this.WED_INTERVAL;
     }
 
+    /**
+    * Gets the queue delay
+    * 
+    * @return the queue delay
+    * @pre $none
+    * @post $none
+    */
     public Map<Integer, Double> getQueueDelay(){
         return this.QUEUE_DELAY;
     }
+    
+    /**
+    * Gets the postscript delay
+    * 
+    * @return the postscript delay
+    * @pre $none
+    * @post $none
+    */
     public Map<Integer, Double> getPostDelay(){
         return this.POST_DELAY;
     }
+    
+    /**
+    * Gets the workflow engine delay
+    * 
+    * @return the workflow engine delay
+    * @pre $none
+    * @post $none
+    */
     public Map<Integer, Double> getWEDDelay(){
         return this.WED_DELAY;
     }
+    
+    /**
+    * Gets the clustering delay
+    * 
+    * @return the clustering delay
+    * @pre $none
+    * @post $none
+    */
     public Map<Integer, Double> getClustDelay(){
         return this.CLUST_DELAY;
     }
     
+    /**
+    * Gets the clustering delay for a particular job based on the depth(level)
+    * 
+    * @param  cl, the job 
+    * @return the clustering delay
+    * @pre $none
+    * @post $none
+    */
     public double getClustDelay(Cloudlet cl)
     {
         double delay = 0.0;
@@ -112,6 +186,14 @@ public class OverheadParameters {
         return delay;
     }  
     
+    /**
+    * Gets the queue delay for a particular job based on the depth(level)
+    * 
+    * @param  cl, the job 
+    * @return the queue delay
+    * @pre $none
+    * @post $none
+    */
     public double getQueueDelay(Cloudlet cl)
     {
         double delay = 0.0;
@@ -133,7 +215,16 @@ public class OverheadParameters {
             Log.printLine("Not yet supported");
         }
         return delay;
-    }        
+    }  
+    
+    /**
+    * Gets the postscript delay for a particular job based on the depth(level)
+    * 
+    * @param  cl, the job 
+    * @return the postscript delay
+    * @pre $none
+    * @post $none
+    */
     public double getPostDelay(Job job)
     {
         double delay = 0.0;
@@ -156,6 +247,15 @@ public class OverheadParameters {
         }
         return delay;
     }       
+    
+    /**
+    * Gets the workflow engine delay for a particular job based on the depth(level)
+    * 
+    * @param  cl, the job 
+    * @return the workflow engine delay
+    * @pre $none
+    * @post $none
+    */
     public double getWEDDelay(List list)
         {
             double delay = 0.0;
