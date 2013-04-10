1cb51071853e7cfbb92f8c8154b675b391063295
Weiwei Chen <wchenpublic@gmail.com>
2013-04-10 15:05:24 -0700

    add comments

--- sources/org/workflowsim/failure/FailureRecord.java
+++ sources/org/workflowsim/failure/FailureRecord.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2007-2008 University Of Southern California
+ *  Copyright 2012-2013 University Of Southern California
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,27 +16,48 @@
 package org.workflowsim.failure;
 
 /**
- *
- * @author chenweiwei
+ * Failure Record is a record with all information of a failure
+ * 
+ * @author Weiwei Chen
+ * @since WorkflowSim Toolkit 1.0
+ * @date Apr 9, 2013
  */
 public class FailureRecord {
     
+    /** Length */
     public double length;
     
+    /** number of failed tasks. */
     public int failedTasksNum;
     
+    /** the depth (also used as type in some cases) of a failure. */
     public int depth;//used as type
     
+    /** all the tasks (failed or not). */
     public int allTaskNum;
     
+    /** the location. */
     public int vmId;
     
+    /** the job id. */
     public int jobId;
     
+    /** the workflow id (user id in this version). */
     public int workflowId;
     
+    /** delay length. */
     public double delayLength;
     
+    /**
+     * Initialize a Failure Record
+     * @param length, length of this task
+     * @param tasks, number of failed tasks
+     * @param depth, depth of the failed tasks
+     * @param all, all the tasks
+     * @param vm, vm id where the failure generates
+     * @param job, job id where the failure generates
+     * @param workflow , workflow id where the failure generates
+     */
     public FailureRecord(double length, int tasks, int depth, int all, int vm, int job, int workflow){
         this.length = length;
         this.failedTasksNum = tasks;
