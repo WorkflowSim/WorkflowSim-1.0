c073eabc826732e75c0297e2bb080841b25e69e2
Weiwei Chen <wchenpublic@gmail.com>
2013-04-09 17:31:24 -0700

    add comments

--- sources/org/workflowsim/Task.java
+++ sources/org/workflowsim/Task.java
@@ -65,16 +65,16 @@ public class Task extends Cloudlet{
      */
     private String type;
     
-	/**
-	 * Allocates a new Task object. The task length should be greater than or equal to 1. 
-	 * 
-	 * @param taskId the unique ID of this Task
-	 * @param taskLength the length or size (in MI) of this task to be executed in a
-	 *            PowerDatacenter
-	 * @pre cloudletID >= 0
-	 * @pre cloudletLength >= 0.0
-	 * @post $none
-	 */
+    /**
+     * Allocates a new Task object. The task length should be greater than or equal to 1. 
+     * 
+     * @param taskId the unique ID of this Task
+     * @param taskLength the length or size (in MI) of this task to be executed in a
+     *            PowerDatacenter
+     * @pre taskId >= 0
+     * @pre taskLength >= 0.0
+     * @post $none
+     */
     
     public Task(
                     final int taskId,
