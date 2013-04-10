4966da9158e78103ea4f521457bd08bb3ed0cf71
Weiwei Chen <wchenpublic@gmail.com>
2013-04-10 13:40:10 -0700

    add comments

--- sources/org/workflowsim/clusering/BasicClustering.java
+++ sources/org/workflowsim/clusering/BasicClustering.java
@@ -15,105 +15,140 @@
  */
 package org.workflowsim.clusering;
 
-import org.workflowsim.Job;
-import org.workflowsim.Task;
-//import edu.isi.pegasus.workflowsim.TaskFile;
-import org.workflowsim.utils.Parameters;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import org.workflowsim.Job;
+import org.workflowsim.Task;
+import org.workflowsim.utils.Parameters;
 
 /**
  * The default clustering does no clustering at all, just map a task to a job
+ * 
  * @author Weiwei Chen
+ * @since WorkflowSim Toolkit 1.0
+ * @date Apr 9, 2013
  */
 public class BasicClustering implements ClusteringInterface{
     
+    /** The task list. */
     private List<Task> taskList;
     
+    /** The job list. */
     private List<Job> jobList;
     
+    /** maps from task to its job. */
     private Map mTask2Job;
     
+    /** All the files. */
     private List<org.cloudbus.cloudsim.File> allFileList;
     
-    //private long inputDataSize;
-    
+    /** The root task. */
     private Task root;
     
+    /** the id index. */
     private int idIndex;
+    
+    /**
+     * Gets the files
+     * @return files
+     */
     @Override
     public final List<org.cloudbus.cloudsim.File> getTaskFiles(){
         return this.allFileList;
     }
     
-//    @Override
-//    public final long getInputDataSize(){
-//        return this.inputDataSize;
-//    }
-    
-    
+    /**
+     * Initialize a BasicClustering object
+     */
     public BasicClustering()
     {
         this.jobList        = new ArrayList<Job>();
         this.taskList       = new ArrayList<Task>();
         this.mTask2Job      = new HashMap<Task, Job>();
-        
-        this.allFileList    = new ArrayList<org.cloudbus.cloudsim.File>();
-        //this.inputDataSize  = 0;
-        
+        this.allFileList    = new ArrayList<org.cloudbus.cloudsim.File>();       
         this.idIndex        = 0;
         this.root           = null;
     }
     
+    /**
+     * Sets the task list
+     * @param list task list
+     */
     @Override
     public final void setTaskList(List<Task> list){
         this.taskList = list;
     }
     
+    /**
+     * Gets the job list
+     * @return job list
+     */
     @Override
     public final List<Job> getJobList(){
         return this.jobList;
     }
     
+    /**
+     * Gets the task list
+     * @return task list
+     */
     @Override
     public final List<Task>getTaskList(){
         return this.taskList;
     }
     
+    /**
+     * Gets the map from task to job
+     * @return map
+     */
     public final Map getTask2Job(){
         return this.mTask2Job;
     }
+    
+    /**
+     * The main function of BasicClustering
+     */
     @Override
     public void run(){
         getTask2Job().clear();
         for(Iterator it = getTaskList().iterator();it.hasNext();)
         {
             Task task = (Task)it.next();
-            //twist here
             List taskList = new ArrayList();
             taskList.add(task);
             Job job = addTasks2Job(taskList);
             getTask2Job().put(task, job);
             
         }
+        /** Handle the dependencies issue. */
         updateDependencies();
         
     }
+    
+    /**
+     * Add a task to a new job
+     * @param task the task
+     * @return job the newly created job
+     */
     protected final Job addTasks2Job(Task task){
         ArrayList job = new ArrayList();
         job.add(task);
         return addTasks2Job(job);
     }
+    
+    /**
+     * Add a list of task to a new job
+     * @param taskList the task list
+     * @return job the newly created job
+     */
     protected final Job addTasks2Job(List taskList){
         if(taskList!=null && !taskList.isEmpty())
         {
-            //Log.printLine("Size:" + taskList.size());
             int length = 0;
-            //long inputFileSize =0;
-            //long outputFileSize =0;
+
             int userId = 0;
             int priority = 0;
             int depth = 0;
@@ -123,62 +158,36 @@ public class BasicClustering implements ClusteringInterface{
             {
                 Task task = (Task)it.next();
                 length += task.getCloudletLength();
-                //no use at all
-                //inputFileSize += task.getCloudletFileSize();
-                //outputFileSize += task.getCloudletOutputSize();
+                
                 userId      = task.getUserId();
                 priority    = task.getPriority();
                 depth       = task.getDepth();
                 List fileList = task.getFileList();
                 job.getTaskList().add(task);
-                //job.getFileList().addAll(fileList);
                 
                 getTask2Job().put(task, job);
-                //both
                 for(Iterator itc= fileList.iterator(); itc.hasNext();)
                 {
                     org.cloudbus.cloudsim.File file = (org.cloudbus.cloudsim.File)itc.next();
-                    //if(!WorkflowPlanner.FileName2File.containsKey(file.getName())){
-                    //    WorkflowPlanner.FileName2File.put(file.getName(), file);
-                    //}
-                    //ReplicaCatalog.setFile(file.getName(), file);
+
                     boolean hasFile = false;
-                    /**
-                     * Make sure we have only one input instance of file
-                     */
                    
                     hasFile = job.getFileList().contains(file);
-                     /*
-                    for(Iterator itd = job.getFileList().iterator(); itd.hasNext();)
-                    {
-                        org.cloudbus.cloudsim.File pFile = (org.cloudbus.cloudsim.File)itd.next();
-                        if(pFile.getName().equals(file.getName()))
-                        {
-                            hasFile = true;
-                            break;
-                        }
-                    }*/
+
                     if(!hasFile){
                         
                         job.getFileList().add(file);
                         if(file.getType() == 1){
-                        //if(file.getInout().equals("input")){
-                            //inputFileSize += file.getSize();
-                            
                             //for stag-in jobs to be used
                             if(!this.allFileList.contains(file)){
                                 this.allFileList.add(file);
-                                //this.inputDataSize += file.getSize();
                             }
                         }else 
                             if(file.getType() == 2){
-                            //if(file.getInout().equals("output")){
                                 this.allFileList.add(file);
-                                //outputFileSize += file.getSize();
                         }
                     }
                 }
-                //next
                 for(Iterator itc = task.getRequiredFiles().iterator();itc.hasNext();){
                     String fileName = (String)itc.next();
                     
@@ -190,10 +199,7 @@ public class BasicClustering implements ClusteringInterface{
                 
             }
             
-//            job.setInputDataSize(inputFileSize);
-//            job.setOutputDataSize(outputFileSize);
-            
-            //because a bug of cloudsim I have to redo it here
+
             job.setCloudletLength(length);
             job.setUserId(userId);
             job.setDepth(depth);
@@ -206,6 +212,10 @@ public class BasicClustering implements ClusteringInterface{
                                  
         return null;
     }
+    
+    /**
+     * For a clustered job, we should add clustering delay (by default it is zero)
+     */
     public void addClustDelay(){
         
         for(Iterator it = getJobList().iterator();it.hasNext();){
@@ -213,13 +223,17 @@ public class BasicClustering implements ClusteringInterface{
             
                 
                 double delay = Parameters.getOverheadParams().getClustDelay(job);
-                delay *= 1000;
+                delay *= 1000; // the same ratio used when you parse a workflow
                 long length = job.getCloudletLength();
                 length += (long)delay;
                 job.setCloudletLength(length);
-            
+
         }
     }
+    
+    /**
+     * Update the dependency issues between tasks/jobs
+     */
     protected final void updateDependencies()
     {
         for(Iterator it = getTaskList().iterator();it.hasNext();)
@@ -249,6 +263,7 @@ public class BasicClustering implements ClusteringInterface{
         getTaskList().clear();
     }
     /*
+     * Add a fake root task
      * If you have used addRoot, please use clean() after that
      */
     public Task addRoot(){
@@ -263,13 +278,15 @@ public class BasicClustering implements ClusteringInterface{
                     root.addChild(node);
                 }
             }
-            //it's not sure here
             taskList.add(root);
             
         }
         return root;
     
     }
+    /**
+     * Delete the root task
+     */
     public void clean(){
         if(root!=null){
             for(int i = 0; i < root.getChildList().size();i++){
