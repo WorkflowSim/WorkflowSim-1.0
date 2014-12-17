00cbea7bd5d3ba072c85fe227eae995d94393670
cciis2013 <chenwwforhome@gmail.com>
2014-12-16 20:47:18 -0800

    fix a bug #10

--- sources/org/workflowsim/WorkflowDatacenter.java
+++ sources/org/workflowsim/WorkflowDatacenter.java
@@ -429,7 +429,7 @@ public class WorkflowDatacenter extends Datacenter {
             }
         }
         if (requiredFileStagein && maxBwth > 0.0) {
-            time = file.getSize() / Consts.MILLION * 8 / maxBwth;
+            time = file.getSize() / (double) Consts.MILLION * 8 / maxBwth;
         }
         return time;
     }
